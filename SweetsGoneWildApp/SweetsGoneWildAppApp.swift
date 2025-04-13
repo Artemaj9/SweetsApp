c50817980fb2094cc0a930189aec3a6c66f332b7
ArtemMoiseev <moiseev@physics.msu.ru>
2025-04-13 13:55:41 +0300

    Initial Commit

---
+++ SweetsGoneWildApp/SweetsGoneWildAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetsGoneWildAppApp.swift
+//  SweetsGoneWildApp
+//
+//  Created by Artem on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SweetsGoneWildAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
